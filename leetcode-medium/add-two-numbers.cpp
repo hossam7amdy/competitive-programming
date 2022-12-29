295c11c806fea0e28113a975ce49093a264981a3
Hossam Hamdy <38383270+hossam7amdy@users.noreply.github.com>
2022-12-29 14:47:32 +0200

    Runtime 29ms Beats 95.53% - Memory 71MB Beats 93.83%

---
+++ leetcode-medium/add-two-numbers.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+#Approach: normal sum with carful carry handling
+
+#Complexity
+    - Time: O(max(n,m))
+    - Space: O(max(n,m))
+*/
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode* head = new ListNode(-1);
+
+    bool carry = 0;
+    ListNode* cursor = head;
+    while(l1 || l2){
+        int left = l1 ? l1->val : 0;
+        int right = l2 ? l2->val : 0;
+        int sum = left + right + carry;
+
+        carry = sum > 9;
+        if(carry) sum -= 10;
+    
+        cursor->next = l1 ? l1 : l2;
+        cursor = cursor->next;
+        cursor->val = sum;
+
+        if(l1) l1 = l1->next;
+        if(l2) l2 = l2->next;
+    }
+
+    if(carry)
+        cursor->next = new ListNode(1);
+    return head->next;
+}
+
+int main(){
+
+    return 0;
+}
